6b4d81438d0e40291745977432cedf65d2dd341a
Been Chang Ho <ahrocksil@gmail.com>
2025-02-14 00:46:10 +0900

    0단계 - 기본 코드 준비 (#325)
    * 기존 소스 삭제
    
    * 이전 미션 소스 추가

--- src/test/java/kitchenpos/Fixtures.java
+++ src/test/java/kitchenpos/Fixtures.java
@@ -1,14 +1,10 @@
 package kitchenpos;
 
-import kitchenpos.eatinorders.domain.Order;
-import kitchenpos.eatinorders.domain.OrderLineItem;
-import kitchenpos.eatinorders.domain.OrderStatus;
-import kitchenpos.eatinorders.domain.OrderTable;
-import kitchenpos.eatinorders.domain.OrderType;
-import kitchenpos.menus.domain.Menu;
-import kitchenpos.menus.domain.MenuGroup;
-import kitchenpos.menus.domain.MenuProduct;
-import kitchenpos.products.domain.Product;
+import kitchenpos.eatinorder.domain.model.*;
+import kitchenpos.menu.domain.model.Menu;
+import kitchenpos.menu.domain.model.MenuGroup;
+import kitchenpos.menu.domain.model.MenuProduct;
+import kitchenpos.product.domain.model.Product;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
